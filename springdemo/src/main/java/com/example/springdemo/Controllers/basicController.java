36a21ce8eae4a4aa7f800bf9842a28a592015376
aryan1403 <aaryan14032006@email.com>
2024-06-11 16:22:20 +0530

    post method

--- springdemo/src/main/java/com/example/springdemo/Controllers/basicController.java
+++ springdemo/src/main/java/com/example/springdemo/Controllers/basicController.java
@@ -3,12 +3,13 @@ package com.example.springdemo.Controllers;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.springdemo.Models.emp;
+import com.example.springdemo.Models.loginModel;
 
 import java.util.UUID;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class basicController {
@@ -17,6 +18,21 @@ public class basicController {
     public String welcome() {
         return "Welcome to my Spring application";
     }
+
+    @PostMapping("/postData")
+    public String postReqDemo(@RequestBody String name) {
+        return name;
+    }
+    
+    @PostMapping("/login")
+    public String login(@RequestBody loginModel model) {
+        if(model.getEmail().equals("aryan@xyz.com") 
+        && model.getPass().equals("123")) {
+            return "Login successfull";
+        }
+        return "Login failed";
+    }
+    
 
     @GetMapping("/home")
     public String home() {
---
+++ springdemo/src/main/java/com/example/springdemo/Models/loginModel.java
@@ -0,0 +1,9 @@
+package com.example.springdemo.Models;
+
+import lombok.Data;
+
+@Data
+public class loginModel {
+    private String email;
+    private String pass;
+}
